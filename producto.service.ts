194582c11c65314c67127b48e0114fbd663a51db
Jesica Aguirre <jesiagr@gmail.com>
2026-05-20 15:27:28 -0300

    Delete producto.service.ts

--- producto.service.ts
+++
@@ -1,9 +0,0 @@
-@Injectable({ providedIn: 'root' })
-export class ProductoService {
-  private apiUrl = 'http://localhost:8080/api/productos';
-  constructor(private http: HttpClient) {}
-
-  getProductos(): Observable<Producto[]> {
-    return this.http.get<Producto[]>(this.apiUrl);
-  }
-}
